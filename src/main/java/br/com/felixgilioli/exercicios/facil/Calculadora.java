9516037f6380c55aa6bc4a62bdc37e8012b18b97
Felix.Gilioli <felix.gilioli@senior.com.br>
2021-05-10 21:09:27 -0300

    first commit

---
+++ src/main/java/br/com/felixgilioli/exercicios/facil/Calculadora.java
@@ -0,0 +1,43 @@
+package br.com.felixgilioli.exercicios.facil;
+
+public class Calculadora {
+
+    /**
+     * Deve calcular a média do array de números inteiros e retorna-la.
+     * @param ints array de números inteiros.
+     * @return média dos inteiros.
+     */
+    public double getMedia(int[] ints) {
+        return 0;
+    }
+
+    /**
+     * Deve retornar qual o maior número dentre {@param a} e {@param b}.
+     * @param a primeiro número.
+     * @param b segundo número.
+     * @return o maior número.
+     */
+    public int getMaior(int a, int b) {
+        return 0;
+    }
+
+    /**
+     * Deve retornar a raíz quadrada do inteiro recebido.
+     * @param n número inteiro.
+     * @return raíz quadrada do {@param n}.
+     */
+    public double getRaizQuadrada(int n) {
+        return 0;
+    }
+
+    /**
+     * Deve retornar o valor absoluto do inteiro {@param n}, ou seja,
+     * se ele for um número negativo deve tranforma-lo para positivo.
+     * @param n número inteiro.
+     * @return valor absoluto.
+     */
+    public int getAbsoluto(int n) {
+        return 0;
+    }
+
+}
