b3b13d76e5a6e346c2898c5a9818d25c9c6d787c
felixgilioli <felix_gilioli@hotmail.com>
2021-05-12 18:04:18 -0300

    LocalDates

---
+++ src/main/java/br/com/felixgilioli/exercicios/facil/ArrayLists.java
@@ -0,0 +1,20 @@
+package br.com.felixgilioli.exercicios.facil;
+
+import java.util.List;
+
+/**
+ * Classe com métodos para trabalhar com listas do tipo {@link java.util.ArrayList}.
+ */
+public class ArrayLists {
+
+    private ArrayLists() {}
+
+    /**
+     * Deve filtrar os elementos de {@param numeros} e retornar apenas os números que são impares.
+     * @param numeros lista de inteiros.
+     * @return lista de inteiros apenas com números impares.
+     */
+    public static List<Integer> getImpares(List<Integer> numeros) {
+        return numeros;
+    }
+}
---
+++ src/test/java/br/com/felixgilioli/exercicios/facil/ArrayListsTest.java
@@ -0,0 +1,41 @@
+package br.com.felixgilioli.exercicios.facil;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ArrayListsTest {
+
+    @Test
+    void getImpares_test01() {
+        List<Integer> numeros = new ArrayList<>(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        List<Integer> impares = ArrayLists.getImpares(numeros);
+        assertEquals(asList(1, 3, 5, 7, 9), impares);
+    }
+
+    @Test
+    void getImpares_test02() {
+        List<Integer> numeros = new ArrayList<>(asList(2, 4, 6, 8, 10));
+        List<Integer> impares = ArrayLists.getImpares(numeros);
+        assertTrue(impares.isEmpty());
+    }
+
+    @Test
+    void getImpares_test03() {
+        List<Integer> numeros = new ArrayList<>(asList(1, 3, 5));
+        List<Integer> impares = ArrayLists.getImpares(numeros);
+        assertEquals(asList(1, 3, 5), impares);
+    }
+
+    @Test
+    void getImpares_test04() {
+        List<Integer> numeros = new ArrayList<>();
+        List<Integer> impares = ArrayLists.getImpares(numeros);
+        assertTrue(impares.isEmpty());
+    }
+}
